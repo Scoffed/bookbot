fbc76f6c86cc445bb89b40cab59d92f74d7a3404
Scoffed <valihavasi@gmail.com>
2025-04-01 14:46:03 +0300

    add sort_dict function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_char
+from stats import sort_dicts
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -11,9 +12,17 @@ def get_book_text(path_to_file):
 def main(file_path="books/frankenstein.txt"):
     num_words = count_words(get_book_text(file_path))
     char_nums = count_char(get_book_text(file_path))
-    print(f"{num_words} words found in the document")
-    print(char_nums)
+    report = sort_dicts(char_nums)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    print(report.isalpha())
+    # print(char_nums)
 
+#     Import and call the function in main.py, and capture the result.
+#     Print the report to the console as shown above. If the character is not an alphabetical character (using the .isalpha() method), just skip it.
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,5 @@
-# Add a new function to your stats.py file that takes the text from the book as a string, and returns the number of times each character, (including symbols and spaces), appears in the string.
 
-#     Convert any character to lowercase using the .lower() method, we don't want duplicates.
-#     Use a dictionary of String -> Integer. The returned dictionary should look something like this:
+
 
 
 def count_words(text):
@@ -18,3 +16,34 @@ def count_char(text):
         else:
             char_count_dict[char] = 1
     return char_count_dict
+
+#     Add a new function to your stats.py file that takes the dictionary of characters and their counts and returns a sorted list of dictionaries.
+#         Each dictionary should have two key-value pairs: one for the character itself and one for that character's count.
+#         Sort from greatest to least by the count.
+#         The .sort() method will be helpful here (see the hint).
+#     Import and call the function in main.py, and capture the result.
+#     Print the report to the console as shown above. If the character is not an alphabetical character (using the .isalpha() method), just skip it.
+
+# Run and submit the CLI tests.
+# Hint
+
+# Here's an example of how you can use the .sort() method to sort a list of dictionaries:
+
+# # A function that takes a dictionary and returns the value of the "num" key
+# # This is how the `.sort()` method knows how to sort the list of dictionaries
+# def sort_on(dict):
+#     return dict["num"]
+
+# vehicles = [
+#     {"name": "car", "num": 7},
+#     {"name": "plane", "num": 10},
+#     {"name": "boat", "num": 2}
+# ]
+# vehicles.sort(reverse=True, key=sort_on)
+# print(vehicles)
+# # [{'name': 'plane', 'num': 10}, {'name': 'car', 'num': 7}, {'name': 'boat', 'num': 2}]
+
+def sort_dict(dictionar):
+    dict_list = []
+    for kv in dictionar:
+        dict_list.append(kv)
