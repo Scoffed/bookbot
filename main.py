548c09cd1bc6cb97452af52898e6573c57b709f3
Scoffed <valihavasi@gmail.com>
2025-04-01 15:53:18 +0300

    Print a report

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 from stats import count_words
 from stats import count_char
-from stats import sort_dicts
+from stats import sort_dict
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
+# print(char_nums)
+#     Import and call the function in main.py, and capture the result.
+#     Print the report to the console as shown above. If the character is not an alphabetical character (using the .isalpha() method), just skip it.
+
+
 
     
 def main(file_path="books/frankenstein.txt"):
     num_words = count_words(get_book_text(file_path))
     char_nums = count_char(get_book_text(file_path))
-    report = sort_dicts(char_nums)
+    report = sort_dict(char_nums)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    print(report.isalpha())
-    # print(char_nums)
+    for char_dict in report:
+        character = char_dict["name"]
+        count = char_dict["num"]
+        print(f"{character}: {count}")
 
-#     Import and call the function in main.py, and capture the result.
-#     Print the report to the console as shown above. If the character is not an alphabetical character (using the .isalpha() method), just skip it.
 
 
 main()
     
-    
+    #     Import and call the function in main.py, and capture the result.
+#     Print the report to the console as shown above. If the character is not an alphabetical character (using the .isalpha() method), just skip it.
+
+# Run and submit the CLI tests.
+# Hint
+
+# Here's an example of how you can use the .sort() method to sort a list of dictionaries:
+
+# # A function that takes a dictionary and returns the value of the "num" key
+# # This is how the `.sort()` method knows how to sort the list of dictionaries
+# def sort_on(dict):
+#     return dict["num"]
+
+# vehicles = [
+#     {"name": "car", "num": 7},
+#     {"name": "plane", "num": 10},
+#     {"name": "boat", "num": 2}
+# ]
+# vehicles.sort(reverse=True, key=sort_on)
+# print(vehicles)
+# # [{'name': 'plane', 'num': 10}, {'name': 'car', 'num': 7}, {'name': 'boat', 'num': 2}]
--- stats.py
+++ stats.py
@@ -11,39 +11,24 @@ def count_words(text):
 def count_char(text):
     char_count_dict = {}
     for char in text.lower():
-        if char in char_count_dict:
-            char_count_dict[char] += 1
-        else:
-            char_count_dict[char] = 1
+        if char.isalpha():
+            if char in char_count_dict:
+                char_count_dict[char] += 1
+            else:
+                char_count_dict[char] = 1
     return char_count_dict
 
-#     Add a new function to your stats.py file that takes the dictionary of characters and their counts and returns a sorted list of dictionaries.
-#         Each dictionary should have two key-value pairs: one for the character itself and one for that character's count.
-#         Sort from greatest to least by the count.
-#         The .sort() method will be helpful here (see the hint).
-#     Import and call the function in main.py, and capture the result.
-#     Print the report to the console as shown above. If the character is not an alphabetical character (using the .isalpha() method), just skip it.
-
-# Run and submit the CLI tests.
-# Hint
-
-# Here's an example of how you can use the .sort() method to sort a list of dictionaries:
-
-# # A function that takes a dictionary and returns the value of the "num" key
-# # This is how the `.sort()` method knows how to sort the list of dictionaries
-# def sort_on(dict):
-#     return dict["num"]
-
-# vehicles = [
-#     {"name": "car", "num": 7},
-#     {"name": "plane", "num": 10},
-#     {"name": "boat", "num": 2}
-# ]
-# vehicles.sort(reverse=True, key=sort_on)
-# print(vehicles)
-# # [{'name': 'plane', 'num': 10}, {'name': 'car', 'num': 7}, {'name': 'boat', 'num': 2}]
-
+def sort_on(dict):
+        return dict["num"]
+    
 def sort_dict(dictionar):
     dict_list = []
+
     for kv in dictionar:
-        dict_list.append(kv)
+        dict_char = {}
+        dict_char["name"] = kv
+        dict_char["num"] = dictionar[kv]
+        dict_list.append(dict_char)
+        
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
